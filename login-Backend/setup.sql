f1a3662a483837f6db3b91313917cf58fb3c5590
Sai Aditya Patil <140183308+Saiaditya004@users.noreply.github.com>
2024-07-21 15:49:26 +0530

    create table commands

--- login-Backend/setup.sql
+++ login-Backend/setup.sql
@@ -4,3 +4,21 @@ CREATE TABLE users (
     password VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS games (
+    event_id INTEGER,
+    game_id BIGINT PRIMARY KEY,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    result VARCHAR(50),
+    white_player_id INTEGER,
+    black_player_id INTEGER,
+    moves TEXT
+);
+
+CREATE TABLE moves (
+    move_id BIGINT PRIMARY KEY,
+    game_id BIGINT,
+    player_id INTEGER,
+    move_number INTEGER,
+    move VARCHAR(255)  -- Increased length to 255
+);
